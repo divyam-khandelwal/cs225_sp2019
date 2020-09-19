9450f8b109a1edd50db45ce3b09ad0e6f3484e2b
Divyam Khandelwal <Kdivyam@gmail.com>
2020-09-19 18:12:21 -0500

    Added HSLPAPixel files

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -3,9 +3,42 @@
  * Implementation of the HSLAPixel class for use in with the PNG library.
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
-namespace cs225 {
-}
+namespace cs225
+{
+
+    HSLAPixel::HSLAPixel()
+    {
+        l = 1.0;
+        a = 1.0;
+    }
+
+    HSLAPixel::HSLAPixel(double hue,
+                         double saturation,
+                         double luminance)
+    {
+        a = 1.0;
+        h = hue;
+        l = luminance;
+        s = saturation;
+    }
+
+    HSLAPixel::HSLAPixel(double hue,
+                         double saturation,
+                         double luminance,
+                         double alpha)
+    {
+        a = alpha;
+        h = hue;
+        l = luminance;
+        s = saturation;
+    }
+
+} // namespace cs225
--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -2,9 +2,62 @@
  * @file HSLAPixel.h
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #pragma once
 
-namespace cs225 {
+#include <iostream>
+#include <sstream>
+
+namespace cs225
+{
+    class HSLAPixel
+    {
+    public:
+        /**
+              * Constructs a default HSLAPixel.
+              * 
+              * A default pixel is completely opaque (non-transparent) and white. 
+              * Opaque implies that the alpha component of the pixel is 1.0. 
+              * Lower alpha values are (semi-)transparent. 
+              */
+        HSLAPixel();
+
+        /**
+              * Constructs an opaque HSLAPixel with the given hue, saturation, and luminance values. 
+              * 
+              * @param hue Hue value for the new pixel, in degrees [0, 360).  
+              * @param saturation Saturation value for the new pixel, [0, 1]. 
+              * @param luminance Luminance value for the new pixel, [0, 1]. 
+              */
+        HSLAPixel(double hue,
+                  double saturation,
+                  double luminance);
+
+        /**
+              * Constructs an HSLAPixel with the given hue, saturation, luminance, and alpha values.  
+              * 
+              * @param hue Hue value for the new pixel, in degrees [0, 360).  
+              * @param saturation Saturation value for the new pixel, [0, 1]. 
+              * @param luminance Luminance value for the new pixel, [0, 1]. 
+              * @param alpha Alpha value for the new pixel, [0, 1]. 
+              */
+        HSLAPixel(double hue,
+                  double saturation,
+                  double luminance,
+                  double alpha);
+
+        // Alpha of the pixel, [0, 1].
+        double a;
+
+        // Hue of the pixel, in degrees [0, 360).
+        double h;
+
+        // Luminance of the pixel, [0, 1].
+        double l;
+
+        // Saturation of the pixel, [0, 1].
+        double s;
+    };
 }
