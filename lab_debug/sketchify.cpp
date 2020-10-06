c8fd7f06bdb91f82c9fe04b069d9d99fc4b4fa1e
Divyam Khandelwal <Kdivyam@gmail.com>
2020-10-05 19:05:16 -0500

    Lab Debug complete

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -29,29 +29,29 @@ PNG* setupOutput(unsigned w, unsigned h) {
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(-1, 0.8, 0.5);
-    return &p;
+    HSLAPixel* p = new HSLAPixel(280, 0.8, 0.5);
+    return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
+    PNG* original = new PNG();
 
     original->readFromFile(inputFile);
-    unsigned width = original->width();
-    unsigned height = original->height();
+    int width = original->width();
+    int height = original->height();
 
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
+
 
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor();
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (int y = 1; y < height; y++) {
+        for (int x = 1; x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -59,7 +59,7 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel& currOutPixel = output->getPixel(x, y);
             if (diff > 20) {
                 currOutPixel = *myPixel;
             }
